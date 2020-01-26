1c13c5a053b12913147508961fd964d1afd7764a
thomascbyrne12 <47927482+thomascbyrne12@users.noreply.github.com>
2020-01-26 12:22:32 -0500

    Update

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,42 @@
-const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Sets url to the link the json file
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Fetch the JSON data and console log it
-d3.json(url).then(function (earthquake_data) {
-    console.log(earthquake_data);
+d3.json(url, function (earthquake_data) {
+    // Call function that is declared below
+    create_features(earthquake_data.features);
 });
+
+// Initialize Function
+function create_features(earthquake_data) {
+
+    // Function to hold switch statement
+    function eachFeature(feature, layer) {
+        // Initializing Variable
+        var earthquake_day_of_week;
+        // Switch statement to read in data
+        switch (new Date(feature.properties.time).getDay()) {
+            case 0:
+                earthquake_day_of_week = 'Sunday';
+                break;
+            case 1: 
+                earthquake_day_of_week = 'Monday';
+                break;
+            case 2:
+                earthquake_day_of_week = 'Tuesday';
+                break;
+            case 3:
+                earthquake_day_of_week = 'Wednesday';
+                break;
+            case 4:
+                earthquake_day_of_week = 'Thursday';
+                break;
+            case 5:
+                earthquake_day_of_week = 'Friday';
+                break;
+            default:
+                earthquake_day_of_week = 'Saturday';
+                break;
+        }
+    }
+};
