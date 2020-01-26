2face8c84d7fd00b00db32e8df6e7658815de695
thomascbyrne12 <47927482+thomascbyrne12@users.noreply.github.com>
2020-01-26 11:20:52 -0500

    replace

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,6 @@
 const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Fetch the JSON data and console log it
-d3.json(url).then(function (data) {
-    console.log(data);
+d3.json(url).then(function (earthquake_data) {
+    console.log(earthquake_data);
 });
